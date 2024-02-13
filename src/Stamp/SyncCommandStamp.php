30dd2a8e1d1f0a735196d3fb70681014d1ae839b
codaholic <codaholic@macpaw.com>
2024-02-13 09:05:30 +0000

    feat(ORC-362): Symfony Messenger Bundle

---
+++ src/Stamp/SyncCommandStamp.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Macpaw\SymfonyMessengerBundle\Stamp;
+
+use Symfony\Component\Messenger\Stamp\StampInterface;
+
+class SyncCommandStamp implements StampInterface
+{
+}
---
+++ src/SymfonyMessengerBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Macpaw\SymfonyMessengerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SymfonyMessengerBundle extends Bundle
+{
+}
